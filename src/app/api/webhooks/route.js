eead67f6dd44ce47c69e4ca389c1f484f4b4871f
mahmud-777 <mahmud33k@gmail.com>
2025-01-16 00:05:38 +0600

    Add mongodb

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,5 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
+import { clerkClient } from '@clerk/nextjs/dist/types/server'
+import { createOrUpdateUser, deleteUser } from '@/lib/actions/user'
 
 
 export async function POST(req) {
@@ -47,20 +49,61 @@ export async function POST(req) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data
-  const eventType = evt.type
+  const { id } = evt?.data;
+  const eventType = evt?.type;
+
+  if( eventType === 'user.created' || eventType === 'user.updated'){
+    const { first_name, last_name, image_url, email_addresses } = evt?.data;
+    try {
+      const user = await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses
+      );
+      if(user && eventType === 'user.created') {
+        try {
+          const client = await clerkClient();
+          await client.users.updateUserMetadata(id, {
+            publicMetadata: {
+              userMongoId: user._id,
+            },
+          });          
+        } catch (error) {
+          console.log('Error: Could not update user metadata:', error);          
+        }
+      }      
+    } catch (error) {
+      console.log('Error: Could not create or update user:', error);
+      return new Response('Error: Could not create or update user', {
+        status : 400,
+      });
+    }
+  }
+
+  if( eventType === 'user.deleted') {
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.log('Error: Could not delete user:', error); 
+      return new Response('Error: Could not delete user',{
+        status: 400,
+      });
+    }
+  }
   console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
   console.log('Webhook payload:', body)
 
-  if(eventType === 'user.created') {
-    console.log('User created event')
-  }
-  if(eventType === 'user.updated') {
-    console.log('User updated event')
-  }
-  if(eventType === 'user.deleted') {
-    console.log('User deleted event')
-  }
+  // if(eventType === 'user.created') {
+  //   console.log('User created event')
+  // }
+  // if(eventType === 'user.updated') {
+  //   console.log('User updated event')
+  // }
+  // if(eventType === 'user.deleted') {
+  //   console.log('User deleted event')
+  // }
 
 
   return new Response('Webhook received', { status: 200 })
